78cbf942db8bfcf95f30771a7852a9e008644374
Yurii Yanchuk <yurii.a.yanchuk@gmail.com>
2023-12-21 23:23:50 +0200

    Add Controller to View connection plus Parameters

--- src/main/java/edu/yuryan/spring/controllers/FirstController.java
+++ src/main/java/edu/yuryan/spring/controllers/FirstController.java
@@ -3,13 +3,27 @@ package edu.yuryan.spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
-//@RequestMapping ("/prior-link")  -  prior link will be added to all links i.g /prior-link/hello, ! it should be considered in links from view folders!
+@RequestMapping("/prior-link")
+// prior link will be added to all links i.g /prior-link/hello, ! it should be considered in links from view folders!
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+//  *** this variant gets parameters from HttpServletRequest object, which contains a lot of info
+//    (input parameters into address line is not obligatory)
+//    public String helloPage(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        //    (input parameters into address line is obligatory or use required = false)
+
+        System.out.println("Parameters: name = " + name + ", surname = " + surname);
         return "/first/hello";
     }
 
