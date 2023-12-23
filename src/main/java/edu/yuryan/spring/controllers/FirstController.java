264fca6957af0a85c2fd81cfee59c3293b449ebf
Yurii Yanchuk <yurii.a.yanchuk@gmail.com>
2023-12-23 17:48:49 +0200

    Add simple calculator implementation

--- src/main/java/edu/yuryan/spring/controllers/FirstController.java
+++ src/main/java/edu/yuryan/spring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package edu.yuryan.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,15 +21,42 @@ public class FirstController {
 //        String surname = request.getParameter("surname");
 
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
         //    (input parameters into address line is obligatory or use required = false)
 
-        System.out.println("Parameters: name = " + name + ", surname = " + surname);
+//        System.out.println("Parameters: name = " + name + ", surname = " + surname);
+        model.addAttribute("message", "Parameters: name = " + name + ", surname = " + surname);
         return "/first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "/first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String countingResult(@RequestParam(value = "a", required = false) int a,
+                                 @RequestParam(value = "b", required = false) int b,
+                                 @RequestParam(value = "operation", required = false) String operation,
+                                 Model model) {
+        int result = -1;
+        switch (operation) {
+            case "plus":
+                result = a + b;
+                break;
+            case "minus":
+                result = a - b;
+                break;
+            case "multiple":
+                result = a * b;
+                break;
+            case "divide":
+                result = a / b;
+                break;
+        }
+
+        model.addAttribute("countResult", "a " + operation + " b equal to " + result);
+        return "/calculator/result";
     }
 }
